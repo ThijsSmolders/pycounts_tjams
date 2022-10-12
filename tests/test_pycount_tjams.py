292ae5b80261739eb092971a36a2238a39a34ffa
Thijs Smolders <smolders.thijs@gmail.com>
2022-10-12 15:00:22 +0200

    test: add unit test for count_words

--- tests/test_pycount_tjams.py
+++ tests/test_pycount_tjams.py
@@ -1,1 +1,13 @@
-from pycount_tjams import pycount_tjams
+from pycount_tjams.pycount_tjams import count_words
+from collections import Counter
+
+def test_count_words():
+    """Test word counting from a file."""
+    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1,
+                        'the': 1, 'same': 1, 'thing': 1,
+                        'over': 2, 'and': 2, 'expecting': 1,
+                        'different': 1, 'results': 1})
+    actual = count_words("tests/einstein.txt")
+    print(actual)
+    print(expected)
+    assert actual == expected, "Einstein quote counted incorrectly!"
